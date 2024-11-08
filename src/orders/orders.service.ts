35994e494b70c648cb6141e46a926bc8554ef43d
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-11-08 09:53:35 -0300

    added order item

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -14,7 +14,10 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    return this.order.create({ data: createOrderDto });
+    return {
+      service: 'Orders Microservice',
+      createOrderDto,
+    };
   }
 
   async findAll(ordersPaginationDto: OrdersPaginationDto) {
